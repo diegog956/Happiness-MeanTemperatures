a58a42797ec482724aa6cba2a9b0061b542b6735
Diego <diego.gerardo95@hotmail.com>
2024-08-20 11:45:30 -0300

    Add main banner & clean dataframe index

--- main.py
+++ main.py
@@ -7,4 +7,18 @@ from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 
 sources = ['https://www.kaggle.com/datasets/ajaypalsinghlo/world-happiness-report-2021', 'https://en.wikipedia.org/wiki/List_of_countries_by_average_yearly_temperature']
 
-print(1)
+df = pd.DataFrame(pd.read_csv('data\data_happiness_meantemperature.csv'))
+
+st.image('data\winter_vs_summer.jpg')
+
+st.title('Is there a correlation between happiness and warm weather?')
+
+st.text("""It's widely believed that people tend to be happier during the summer, 
+while winter is often associated with sadness.
+        
+Let's explore the data from the 2021 World Happiness Report alongside the average annual
+temperatures of countries around the world to see what insights we can uncover.\n""")
+
+
+
+st.dataframe(df)
